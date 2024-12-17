df426318c9821bf54b6ff8534e9d2b5e0e21b3ac
Stephen Davis <steve@danlargo.com>
2024-12-17 13:25:23 -0500

    Added AVFoundation interface objects, enumeration of cameras

---
+++ macos/v4l2cam/ObjCCamera.mm
@@ -0,0 +1,18 @@
+//
+//  ObjCCamea.mm
+//  v4l2cam-macos
+//
+//  Created by Stephen Davis on 2024-12-15.
+//
+
+#include "ObjCCamera.h"
+
+@implementation ObjCCamera
+
++ (int)discoverCameras
+{
+    
+    return 0;
+}
+
+@end
