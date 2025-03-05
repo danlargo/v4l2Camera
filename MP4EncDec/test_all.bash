1c43f9eff789cf5c308ac09707dde5da34b5f1df
Stephen Davis <steve@danlargo.com>
2025-03-05 09:48:38 -0500

    Update makefile

--- MP4EncDec/test_all.bash
+++ MP4EncDec/test_all.bash
@@ -2,15 +2,16 @@
 #
 # test all file variants
 #
-./decodeMP4 -i ./test1.mp4 | grep "????"
-./decodeMP4 -i ./test2.mp4 | grep "????"
-./decodeMP4 -i ./test3.mp4 | grep "????"
-./decodeMP4 -i ./test4.mp4 | grep "????"
-./decodeMP4 -i ./test5.mp4 | grep "????"
-./decodeMP4 -i ./test6.mp4 | grep "????"
-./decodeMP4 -i ./test7.mp4 | grep "????"
-./decodeMP4 -i ./test8.mp4 | grep "????"
-./decodeMP4 -i ./test1.mov | grep "????"
-./decodeMP4 -i ./test2.mov | grep "????"
-./decodeMP4 -i ./test3.mov | grep "????"
+./decodeMP4 -i ./test1.mp4 | grep "??"
+./decodeMP4 -i ./test2.mp4 | grep "??"
+./decodeMP4 -i ./test3.mp4 | grep "??"
+./decodeMP4 -i ./test4.mp4 | grep "??"
+./decodeMP4 -i ./test5.mp4 | grep "??"
+./decodeMP4 -i ./test6.mp4 | grep "??"
+./decodeMP4 -i ./test7.mp4 | grep "??"
+./decodeMP4 -i ./test8.mp4 | grep "??"
+./decodeMP4 -i ./test1.mov | grep "??"
+./decodeMP4 -i ./test2.mov | grep "??"
+./decodeMP4 -i ./test3.mov | grep "??"
+./decodeMP4 -i ./test1.3gp | grep "??"
 
